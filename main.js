b7318dc558425ea752402d52de1c02fbb5e7a5ea
Davy Viana <113125427+DavYapetus@users.noreply.github.com>
2024-07-26 16:40:28 -0300

    Finalização da versao paginada
    organização do js feita com o offset
    css atualizado
    codigo organizado

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-
-var apiUrl = "https://gateway.marvel.com:443/v1/public/characters/1009718/series?ts=1&apikey=0ddb966155a236dfb10abcd76d73c605&hash=7c9aff95111be45f04e565a3f85ca04d&offset=00"
+var strIncremento = 0
+var apiUrl = "https://gateway.marvel.com:443/v1/public/characters/1009718/series?ts=1&apikey=0ddb966155a236dfb10abcd76d73c605&hash=7c9aff95111be45f04e565a3f85ca04d&offset=0"+strIncremento
 
 function pesquisa(){
 fetch(apiUrl)
@@ -11,22 +11,23 @@ fetch(apiUrl)
   })
   .then(data => {
     for (i in data.data.results ){
-        const novaDiv = document.createElement("div");
-        const titulo = document.createElement("h5");
-        const capaDaComic = document.createElement('img');
-        titulo.innerHTML = data.data.results[i].title;
-        if (data.data.results[i].thumbnail.path) {
-          console.log(data.data.results[i].thumbnail.path)
-          capaDaComic.src = data.data.results[i].thumbnail.path + "." + data.data.results[i].thumbnail.extension;}
-        else {
-          console.log(data.data.results[i].images[0].path)
-          capaDaComic.src = data.data.results[i].images.path + "." + data.data.results[i].thumbnail.extension;}
-        console.log(data.data.results[i])
-        capaDaComic.style.height = "480px";
-        capaDaComic.style.width = "320px";
-        novaDiv.appendChild(titulo);
-        novaDiv.appendChild(capaDaComic);
-        document.body.appendChild(novaDiv);
+      const novaDiv = document.createElement("div");
+      novaDiv.setAttribute("id", "comic")
+      const titulo = document.createElement("h5");
+      const capaDaComic = document.createElement('img');
+      titulo.innerHTML = data.data.results[i].title;
+      if (data.data.results[i].thumbnail.path) {
+        console.log(data.data.results[i].thumbnail.path)
+        capaDaComic.src = data.data.results[i].thumbnail.path + "." + data.data.results[i].thumbnail.extension;}
+      else {
+        console.log(data.data.results[i].images[0].path)
+        capaDaComic.src = data.data.results[i].images.path + "." + data.data.results[i].thumbnail.extension;}
+      console.log(data.data.results[i])
+      capaDaComic.style.height = "480px";
+      capaDaComic.style.width = "320px";
+      novaDiv.appendChild(titulo);
+      novaDiv.appendChild(capaDaComic);
+      document.body.insertBefore(novaDiv, botaoProx);
     }
     if (data.data.results.id) outputElement.textContent = JSON.stringify(data.data.results.id, null, 2);
   })
@@ -35,13 +36,24 @@ fetch(apiUrl)
   });
   
 }
+var contador = 0;
 var incremento = 0;
-  botaoPesquisa.onclick = function() {
-      pesquisa()}
-  botaoProx.onclick = function() {
-    incremento += 20;
-    var strIncremento = incremento.toString();
-    apiUrl = apiUrl.slice(0, -2)
-    apiUrl = apiUrl+strIncremento
-    console.log(apiUrl)
-    pesquisa()}
+botaoPesquisa.onclick = function() {
+  pesquisa()}
+botaoProx.onclick = function() {
+  contador += 1
+  tesoura = -2
+  console.log(contador)
+  if (contador > 5){
+    tesoura -=1
+  }
+  if (contador > 50){
+    tesoura -=1
+  }
+  console.log(tesoura)
+  incremento += 20;
+  var strIncremento = incremento.toString();
+  apiUrl = apiUrl.slice(0, tesoura)
+  apiUrl = apiUrl+strIncremento
+  console.log(apiUrl)
+  pesquisa()}
